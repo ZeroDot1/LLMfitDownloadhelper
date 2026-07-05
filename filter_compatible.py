43dd7b73eaec8a22693c44662e299066574f85c7
ZeroDot1 <zerodot1@bk.ru>
2026-07-05 16:20:51 +0200

    docs: add GNU AGPLv3 license header to filter_compatible.py

--- filter_compatible.py
+++ filter_compatible.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+# ==============================================================================
+# Application:   LLMfitDownloadhelper
+# Author:        ZeroDot1
+# Version:       1.7
+# Platform:      Universal (Arch Linux & Ubuntu compatible)
+# License:       GNU AGPLv3 (https://gnu.org)
+#
+# Copyright (C) 2026 ZeroDot1
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# ==============================================================================
+
 import sys
 import json
 import subprocess
